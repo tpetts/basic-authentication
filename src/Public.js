24ee4ffb82147fdccde44f72b891f5682ad9408c
Tia Petts <tpetts@madisoncollege.edu>
2021-11-15 16:33:13 -0600

    Adding logic to Public.js
    Renders the name of the route to the UI and using the Container Component to add some padding and margin.

--- src/Public.js
+++ src/Public.js
@@ -1,3 +1,16 @@
 /**
  * This is the basic route that will be viewable whether or not the user is signed in. 
  */
+
+import React from "react";
+import Container from "./Container";
+
+function Public() {
+    return (
+        <Container>
+            <h1>Public Route</h1>
+        </Container>
+    )
+}
+
+export default Public;
