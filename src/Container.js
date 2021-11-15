56de2b2c513c4dd47595eb2b6e2152ee62dc6e88
Tia Petts <tpetts@madisoncollege.edu>
2021-11-15 16:29:04 -0600

    Adding logic to Container.js
    Using this component, I can now apply consistent styling across the entire application without having to rewrite my styles

--- src/Container.js
+++ src/Container.js
@@ -1,3 +1,20 @@
 /**
  * This file will hold a component to apply reusable styling to other components
  */
+
+import React from "react";
+
+const Container = ({ children }) => (
+    <div style={styles.container}>
+        { children }
+    </div>
+)
+
+const styles = {
+    container: {
+        margin: '0 auto',
+        padding: '50px 100px'
+    }
+}
+
+export default Container;
