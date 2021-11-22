cd9bfd1604215206de3838bff3a58e266701818f
Tia Petts <tpetts@madisoncollege.edu>
2021-11-22 12:09:14 -0600

    Protected.js code cleanup
    Cleaning things up...

--- src/Protected.js
+++ src/Protected.js
@@ -6,16 +6,20 @@
 
 import React, { useEffect } from 'react';
 import { Auth } from 'aws-amplify';
-import Container from './Container';
+import { Container } from './Container';
 
-function Protected(props) {
-    useEffect(() => {
+const Protected = (props) => {
+    useEffect( () => {
+        // if the user is signed in...
         Auth.currentAuthenticatedUser()
+            // if the user isn't signed in...
             .catch(() => {
                 // The user isn't signed in, so we need to redirect them by calling this:
                 props.history.push('/profile')
             })
-    }, []);
+    }, []);// [] = just run this when the component loads, don't run this repeatedly...
+
+
     return (
         <Container>
             <h1>Protected Route</h1>
