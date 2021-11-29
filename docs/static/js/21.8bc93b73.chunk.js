25b86e6f80dacf86bd0e01a935ae4ee1e8122fc5
Tia Petts <tpetts@madisoncollege.edu>
2021-11-29 12:48:29 -0600

    deleted docs
    trying to troubleshoot github pages launch

--- docs/static/js/21.8bc93b73.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpbasic-authentication"]=this["webpackJsonpbasic-authentication"]||[]).push([[21],{342:function(t,n,e){"use strict";e.r(n),e.d(n,"amplify_container",(function(){return c}));var i=e(17),c=function(){function t(t){Object(i.k)(this,t)}return t.prototype.render=function(){return Object(i.i)(i.b,null,Object(i.i)("slot",null))},t}();c.style="amplify-container{display:-ms-flexbox;display:flex;-ms-flex-align:center;align-items:center;-ms-flex-pack:center;justify-content:center;height:100vh}"}}]);
-//# sourceMappingURL=21.8bc93b73.chunk.js.map
